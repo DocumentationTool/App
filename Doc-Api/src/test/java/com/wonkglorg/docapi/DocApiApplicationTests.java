403268cc9dedc58f1530b1deea64c68a7215716c
Dominik.MEIERHOFER <Wonkglorg.18@gmail.com>
2025-02-06 10:42:00 +0100

    Initial Setup

---
+++ Doc-Api/src/test/java/com/wonkglorg/docapi/DocApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wonkglorg.docapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ Doc-Core/src/test/java/com/wonkglorg/doccore/DocCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wonkglorg.doccore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
