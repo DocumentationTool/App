5513a22289ed97f5b9445ced9aa7483bd6c047c3
Dominik.MEIERHOFER <Wonkglorg.18@gmail.com>
2025-03-18 15:46:53 +0100

    working on request visual rewrites

--- document/Doc-Core/src/main/java/com/wonkglorg/doc/core/request/ResourceUpdateRequest.java
+++ document/Doc-Core/src/main/java/com/wonkglorg/doc/core/request/ResourceUpdateRequest.java
@@ -1,47 +1,124 @@
 package com.wonkglorg.doc.core.request;
 
-import com.wonkglorg.doc.core.objects.Tag;
+import com.wonkglorg.doc.core.objects.RepoId;
+import com.wonkglorg.doc.core.objects.TagId;
+import com.wonkglorg.doc.core.objects.UserId;
 
+import java.nio.file.Path;
 import java.util.List;
 
-public class ResourceUpdateRequest{
-	
-	/**
-	 * The id of the repository
-	 */
-	public String repoId;
-	/**
-	 * The path to the resource
-	 */
-	public String path;
-	/**
-	 * The user to update the resource as
-	 */
-	public String userId;
-	
-	/**
-	 * The tags to add to the resource
-	 */
-	public List<String> tagsToAdd;
-	/**
-	 * The tags to remove from the resource
-	 */
-	public List<String> tagsToRemove;
-	/**
-	 * The tags to set on the resource removes any previous tags
-	 */
-	public List<String> tagsToSet;
-	
-	/**
-	 * The category to set on the resource
-	 */
-	public String category;
-	
-	/**
-	 * The data to set on the resource
-	 */
-	public String data;
-	
-	public boolean treatNullsAsValues = false;
-	
+import static com.wonkglorg.doc.core.path.TargetPath.normalizePath;
+
+public class ResourceUpdateRequest {
+
+    /**
+     * The id of the repository
+     */
+    private RepoId repoId;
+    /**
+     * The path to the resource
+     */
+    private Path path;
+    /**
+     * The user to update the resource as
+     */
+    private UserId userId;
+
+    /**
+     * The tags to add to the resource
+     */
+    private List<TagId> tagsToAdd;
+    /**
+     * The tags to remove from the resource
+     */
+    private List<TagId> tagsToRemove;
+    /**
+     * The tags to set on the resource removes any previous tags
+     */
+    private List<TagId> tagsToSet;
+
+    /**
+     * The category to set on the resource
+     */
+    private String category;
+
+    /**
+     * The data to set on the resource
+     */
+    private String data;
+
+    private boolean treatNullsAsValues = false;
+
+
+    public String getRepoId() {
+        return repoId.id();
+    }
+
+    public void setRepoId(String repoId) {
+        this.repoId = RepoId.of(repoId);
+    }
+
+    public String getPath() {
+        return path.toString();
+    }
+
+    public void setPath(String path) {
+        this.path = Path.of(normalizePath(path));
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public List<TagId> getTagsToAdd() {
+        return tagsToAdd;
+    }
+
+    public void setTagsToAdd(List<TagId> tagsToAdd) {
+        this.tagsToAdd = tagsToAdd;
+    }
+
+    public List<TagId> getTagsToRemove() {
+        return tagsToRemove;
+    }
+
+    public void setTagsToRemove(List<TagId> tagsToRemove) {
+        this.tagsToRemove = tagsToRemove;
+    }
+
+    public List<TagId> getTagsToSet() {
+        return tagsToSet;
+    }
+
+    public void setTagsToSet(List<TagId> tagsToSet) {
+        this.tagsToSet = tagsToSet;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public boolean isTreatNullsAsValues() {
+        return treatNullsAsValues;
+    }
+
+    public void setTreatNullsAsValues(boolean treatNullsAsValues) {
+        this.treatNullsAsValues = treatNullsAsValues;
+    }
 }
