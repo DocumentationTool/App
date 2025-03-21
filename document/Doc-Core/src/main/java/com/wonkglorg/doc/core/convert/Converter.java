8b73f8894e0c8eeef06274cc6042db4fd97429ba
Dominik.MEIERHOFER <wonkglorg.18@gmail.com>
2025-03-21 10:28:40 +0100

    Removed Word Converter dependency

--- document/Doc-Core/src/main/java/com/wonkglorg/doc/core/convert/Converter.java
+++ document/Doc-Core/src/main/java/com/wonkglorg/doc/core/convert/Converter.java
@@ -1,10 +1,6 @@
 package com.wonkglorg.doc.core.convert;
 
 
-import com.groupdocs.parser.Parser;
-import com.groupdocs.parser.data.TextReader;
-import com.groupdocs.parser.options.FormattedTextMode;
-import com.groupdocs.parser.options.FormattedTextOptions;
 import com.wonkglorg.doc.core.exception.client.ClientException;
 
 import java.io.IOException;
@@ -12,10 +8,15 @@ import java.nio.file.Path;
 
 public class Converter {
     public static String convertWordToMarkdown(Path pathToWord) throws IOException, ClientException {
+        /*
         try (Parser parser = new Parser(pathToWord.toString())) {
             try (TextReader reader = parser.getFormattedText(new FormattedTextOptions(FormattedTextMode.Markdown))) {
                 return reader.readToEnd();
             }
         }
+
+         */
+        return "";
     }
 }
+
